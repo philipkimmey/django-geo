12867ceb3eac36ebe255ffc0ec5e19b104902a35
Philip Kimmey <philip.kimmey@gmail.com>
2012-07-28 15:08:57 -0700

    improve admin interface for zip codes

--- django_geo/admin.py
+++ django_geo/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from models import ZipCode
 
-admin.site.register(ZipCode)
+class ZipCodeAdmin(admin.ModelAdmin):
+    list_display = ('zip_code', 'latitude', 'longitude', 'state', 'city')
+    list_filter = ('state',)
+    search_fields = ('zip_code', 'state', 'city')
+
+admin.site.register(ZipCode, ZipCodeAdmin)
